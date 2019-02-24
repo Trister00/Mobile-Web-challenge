3d4795e6b7040a4096f8bd45e8c9dc8b327aa8f4
Nassim <nassimelalam@gmail.com>
2019-02-24 16:43:24 +0100

    Add Api Call And Render the Result

--- app/components/App.vue
+++ app/components/App.vue
@@ -1,32 +1,58 @@
 <template>
-    <Page>
-        <ActionBar title="Welcome to NativeScript-Vue!"/>
-        <GridLayout columns="*" rows="*">
-            <Label class="message" :text="msg" col="0" row="0"/>
-        </GridLayout>
-    </Page>
+  <Page>
+    <ActionBar title="Trending Repos"/>
+    <GridLayout columns="*" rows="*">
+      <ListView for="item in Repos">
+        <v-template>
+          <StackLayout>
+            <Label :text="item.name"/>
+            <Label :text="item.description"/>
+            <Label :text="item.owner.login"/>
+            <Image :src="item.owner.avatar_url"/>
+            <Label :text="item.watchers"/>
+          </StackLayout>
+        </v-template>
+      </ListView>
+    </GridLayout>
+  </Page>
 </template>
 
 <script >
-  export default {
-    data() {
-      return {
-        msg: 'Hello World!'
-      }
-    }
+const http = require("http");
+export default {
+  data() {
+    return {
+      msg: "Hello World!",
+      Repos: []
+    };
+  },
+  mounted() {
+    http
+      .getJSON(
+        "https://api.github.com/search/repositories?q=created:>2017-10-22&sort=stars&order=desc"
+      )
+      .then(
+        r => {
+          this.Repos.push(...JSON.parse(JSON.stringify(r)).items);
+        },
+        error => {
+          console.log(error);
+        }
+      );
   }
+};
 </script>
 
 <style scoped>
-    ActionBar {
-        background-color: #53ba82;
-        color: #ffffff;
-    }
+ActionBar {
+  background-color: #53ba82;
+  color: #ffffff;
+}
 
-    .message {
-        vertical-align: center;
-        text-align: center;
-        font-size: 20;
-        color: #333333;
-    }
+.message {
+  vertical-align: center;
+  text-align: center;
+  font-size: 20;
+  color: #333333;
+}
 </style>
